b2fc9bfd06dce02939838c022a07c521b9d23e5e
github username <github email address>
2018-04-09 18:03:22 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+      @author = Author.create(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+    
   end
 
   private
@@ -19,3 +23,8 @@ class AuthorsController < ApplicationController
     params.permit(:name, :email, :phone_number)
   end
 end
+# 1. Create controllers for both models.
+# 1. Create `show`, `new`, `edit`, `create`, and `update` routes for both models.
+# 1. Define controller actions for `show`, `new`, and `edit`.
+# 1. Define the "valid path" for the `create` and `update` controller actions.
+# 1. Define the "invalid path" for the `create` and `update` controller actions.
